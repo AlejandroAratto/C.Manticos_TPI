a487c1e2aeb6800cf5e6acdd84172b668ed716a8
Alejandro Aratto <alearatto2@gmail.com>
2026-06-05 01:42:51 -0300

    Lexer flex v1

--- src/lex.yy.c
+++ src/lex.yy.c
@@ -645,12 +645,12 @@ static char *yy_last_accepting_cpos;
 #define YY_MORE_ADJ 0
 #define YY_RESTORE_YY_MORE_OFFSET
 char *yytext;
-#line 1 ".\\Lexer.l"
+#line 1 ".\\src\\Lexer.l"
 #define INITIAL 0
 /* Lexer para Intérprete Smart Home - Equipo C.mánticos
    UTN FRRE - SSL 2026
 */ 
-#line 8 ".\\Lexer.l"
+#line 8 ".\\src\\Lexer.l"
 #include <stdio.h>
 #include <stdlib.h>
 // #include "y.tab.h" // A descomentar cuando se integre con Yacc/Bison
@@ -810,7 +810,7 @@ YY_DECL
 	register char *yy_cp, *yy_bp;
 	register int yy_act;
 
-#line 38 ".\\Lexer.l"
+#line 38 ".\\src\\Lexer.l"
 
 	/*----- Sección de Reglas ----------------*/
 
@@ -899,305 +899,305 @@ do_action:	/* This label is used only to access EOF actions. */
 
 case 1:
 YY_RULE_SETUP
-#line 41 ".\\Lexer.l"
+#line 41 ".\\src\\Lexer.l"
 { /* Ignoramos comentarios de una linea */ }
 	YY_BREAK
 case 2:
 YY_RULE_SETUP
-#line 42 ".\\Lexer.l"
+#line 42 ".\\src\\Lexer.l"
 { /* Ignoramos espacios en blanco */ }
 	YY_BREAK
 case 3:
 YY_RULE_SETUP
-#line 43 ".\\Lexer.l"
+#line 43 ".\\src\\Lexer.l"
 { linea++; }
 	YY_BREAK
 case 4:
 YY_RULE_SETUP
-#line 44 ".\\Lexer.l"
+#line 44 ".\\src\\Lexer.l"
 { printf("Terminando modo interactivo\n"); exit(0); }
 	YY_BREAK
 /* Palabras Reservadas */
 case 5:
 YY_RULE_SETUP
-#line 47 ".\\Lexer.l"
+#line 47 ".\\src\\Lexer.l"
 { printf("Token: TK_WHEN | Lexema: WHEN\n"); /* return WHEN; */ }
 	YY_BREAK
 case 6:
 YY_RULE_SETUP
-#line 48 ".\\Lexer.l"
+#line 48 ".\\src\\Lexer.l"
 { printf("Token: TK_IF | Lexema: IF\n"); /* return IF; */ }
 	YY_BREAK
 case 7:
 YY_RULE_SETUP
-#line 49 ".\\Lexer.l"
+#line 49 ".\\src\\Lexer.l"
 { printf("Token: TK_THEN | Lexema: THEN\n"); /* return THEN; */ }
 	YY_BREAK
 case 8:
 YY_RULE_SETUP
-#line 50 ".\\Lexer.l"
+#line 50 ".\\src\\Lexer.l"
 { printf("Token: TK_ELSE | Lexema: ELSE\n"); /* return ELSE; */ }
 	YY_BREAK
 case 9:
 YY_RULE_SETUP
-#line 51 ".\\Lexer.l"
+#line 51 ".\\src\\Lexer.l"
 { printf("Token: TK_DO | Lexema: DO\n"); /* return DO; */ }
 	YY_BREAK
 case 10:
 YY_RULE_SETUP
-#line 52 ".\\Lexer.l"
+#line 52 ".\\src\\Lexer.l"
 { printf("Token: TK_END | Lexema: END\n"); /* return END; */ }
 	YY_BREAK
 case 11:
 YY_RULE_SETUP
-#line 53 ".\\Lexer.l"
+#line 53 ".\\src\\Lexer.l"
 { printf("Token: TK_EVERY | Lexema: EVERY\n"); /* return EVERY; */ }
 	YY_BREAK
 case 12:
 YY_RULE_SETUP
-#line 54 ".\\Lexer.l"
+#line 54 ".\\src\\Lexer.l"
 { printf("Token: OP_LOGICO | Lexema: AND\n"); /* return AND; */ }
 	YY_BREAK
 case 13:
 YY_RULE_SETUP
-#line 55 ".\\Lexer.l"
+#line 55 ".\\src\\Lexer.l"
 { printf("Token: OP_LOGICO | Lexema: OR\n"); /* return OR; */ }
 	YY_BREAK
 case 14:
 YY_RULE_SETUP
-#line 56 ".\\Lexer.l"
+#line 56 ".\\src\\Lexer.l"
 { printf("Token: OP_LOGICO | Lexema: NOT\n"); /* return NOT; */ }
 	YY_BREAK
 /* Booleanos, Modos y Colores */
 case 15:
 YY_RULE_SETUP
-#line 59 ".\\Lexer.l"
+#line 59 ".\\src\\Lexer.l"
 { printf("Token: BOOLEANO | Lexema:  %s\n", yytext); /* return BOOLEANO; */ }
 	YY_BREAK
 case 16:
 YY_RULE_SETUP
-#line 60 ".\\Lexer.l"
+#line 60 ".\\src\\Lexer.l"
 { printf("Token: MODO_AIRE | Lexema:  %s\n", yytext); /* return MODO_AIRE; */ }
 	YY_BREAK
 case 17:
 YY_RULE_SETUP
-#line 61 ".\\Lexer.l"
+#line 61 ".\\src\\Lexer.l"
 { printf("Token: VALOR_COLOR | Lexema:  %s\n", yytext); /* return VALOR_COLOR; */ }
 	YY_BREAK
 /* Atributos Dispositivos (Notación de punto) */
 case 18:
 YY_RULE_SETUP
-#line 64 ".\\Lexer.l"
+#line 64 ".\\src\\Lexer.l"
 { printf("Token: ATRIBUTO | Lexema:  estado\n"); /* return ESTADO; */ }
 	YY_BREAK
 case 19:
 YY_RULE_SETUP
-#line 65 ".\\Lexer.l"
+#line 65 ".\\src\\Lexer.l"
 { printf("Token: ATRIBUTO | Lexema:  brillo\n"); /* return BRILLO; */ }
 	YY_BREAK
 case 20:
 YY_RULE_SETUP
-#line 66 ".\\Lexer.l"
+#line 66 ".\\src\\Lexer.l"
 { printf("Token: ATRIBUTO | Lexema:  color\n"); /* return COLOR; */ }
 	YY_BREAK
 case 21:
 YY_RULE_SETUP
-#line 67 ".\\Lexer.l"
+#line 67 ".\\src\\Lexer.l"
 { printf("Token: ATRIBUTO | Lexema:  modo\n"); /* return MODO; */ }
 	YY_BREAK
 case 22:
 YY_RULE_SETUP
-#line 68 ".\\Lexer.l"
+#line 68 ".\\src\\Lexer.l"
 { printf("Token: ATRIBUTO | Lexema:  temp_obj\n"); /* return TEMP_OBJ; */ }
 	YY_BREAK
 case 23:
 YY_RULE_SETUP
-#line 69 ".\\Lexer.l"
+#line 69 ".\\src\\Lexer.l"
 { printf("Token: ATRIBUTO | Lexema:  temp_objetivo\n"); /* return TEMP_OBJETIVO; */ }
 	YY_BREAK
 case 24:
 YY_RULE_SETUP
-#line 70 ".\\Lexer.l"
+#line 70 ".\\src\\Lexer.l"
 { printf("Token: ATRIBUTO | Lexema:  temp_act\n"); /* return TEMP_ACT; */ }
 	YY_BREAK
 case 25:
 YY_RULE_SETUP
-#line 71 ".\\Lexer.l"
+#line 71 ".\\src\\Lexer.l"
 { printf("Token: ATRIBUTO | Lexema:  posicion\n"); /* return POSICION; */ }
 	YY_BREAK
 case 26:
 YY_RULE_SETUP
-#line 72 ".\\Lexer.l"
+#line 72 ".\\src\\Lexer.l"
 { printf("Token: ATRIBUTO | Lexema:  volumen\n"); /* return VOLUMEN; */ }
 	YY_BREAK
 case 27:
 YY_RULE_SETUP
-#line 73 ".\\Lexer.l"
+#line 73 ".\\src\\Lexer.l"
 { printf("Token: ATRIBUTO | Lexema:  mute\n"); /* return MUTE; */ }
 	YY_BREAK
 case 28:
 YY_RULE_SETUP
-#line 74 ".\\Lexer.l"
+#line 74 ".\\src\\Lexer.l"
 { printf("Token: ATRIBUTO | Lexema:  mensaje\n"); /* return MENSAJE; */ }
 	YY_BREAK
 case 29:
 YY_RULE_SETUP
-#line 75 ".\\Lexer.l"
+#line 75 ".\\src\\Lexer.l"
 { printf("Token: ATRIBUTO | Lexema:  email_notif\n"); /* return EMAIL_NOTIF; */ }
 	YY_BREAK
 case 30:
 YY_RULE_SETUP
-#line 76 ".\\Lexer.l"
+#line 76 ".\\src\\Lexer.l"
 { printf("Token: ATRIBUTO | Lexema:  email\n"); /* return ATTR_EMAIL; */ }
 	YY_BREAK
 case 31:
 YY_RULE_SETUP
-#line 77 ".\\Lexer.l"
+#line 77 ".\\src\\Lexer.l"
 { printf("Token: ATRIBUTO | Lexema:  activada\n"); /* return ACTIVADA; */ }
 	YY_BREAK
 case 32:
 YY_RULE_SETUP
-#line 78 ".\\Lexer.l"
+#line 78 ".\\src\\Lexer.l"
 { printf("Token: ATRIBUTO | Lexema:  hora\n"); /* return ATTR_HORA; */ }
 	YY_BREAK
 case 33:
 YY_RULE_SETUP
-#line 79 ".\\Lexer.l"
+#line 79 ".\\src\\Lexer.l"
 { printf("Token: ATRIBUTO | Lexema:  fecha\n"); /* return ATTR_FECHA; */ }
 	YY_BREAK
 /* Sensores (Dispositivos de Entrada) */
 case 34:
 YY_RULE_SETUP
-#line 82 ".\\Lexer.l"
+#line 82 ".\\src\\Lexer.l"
 { printf("Token: SENSOR_TEMPERATURA_ID | Lexema: %s\n", yytext); }
 	YY_BREAK
 case 35:
 YY_RULE_SETUP
-#line 83 ".\\Lexer.l"
+#line 83 ".\\src\\Lexer.l"
 { printf("Token: SENSOR_HUMEDAD_ID | Lexema: %s\n", yytext); }
 	YY_BREAK
 case 36:
 YY_RULE_SETUP
-#line 84 ".\\Lexer.l"
+#line 84 ".\\src\\Lexer.l"
 { printf("Token: SENSOR_LUZ_ID | Lexema: %s\n", yytext); }
 	YY_BREAK
 case 37:
 YY_RULE_SETUP
-#line 85 ".\\Lexer.l"
+#line 85 ".\\src\\Lexer.l"
 { printf("Token: SENSOR_MOVIMIENTO_ID | Lexema: %s\n", yytext); }
 	YY_BREAK
 case 38:
 YY_RULE_SETUP
-#line 86 ".\\Lexer.l"
+#line 86 ".\\src\\Lexer.l"
 { printf("Token: SENSOR_HUMO_ID | Lexema: %s\n", yytext); }
 	YY_BREAK
 case 39:
 YY_RULE_SETUP
-#line 87 ".\\Lexer.l"
+#line 87 ".\\src\\Lexer.l"
 { printf("Token: RELOJ_ID | Lexema: %s\n", yytext); }
 	YY_BREAK
 /* Actuadores (Dispositivos de Salida) */
 case 40:
 YY_RULE_SETUP
-#line 90 ".\\Lexer.l"
+#line 90 ".\\src\\Lexer.l"
 { printf("Token: FOCO_ID | Lexema: %s\n", yytext); }
 	YY_BREAK
 case 41:
 YY_RULE_SETUP
-#line 91 ".\\Lexer.l"
+#line 91 ".\\src\\Lexer.l"
 { printf("Token: AIRE_ID | Lexema: %s\n", yytext); }
 	YY_BREAK
 case 42:
 YY_RULE_SETUP
-#line 92 ".\\Lexer.l"
+#line 92 ".\\src\\Lexer.l"
 { printf("Token: PERSIANA_ID | Lexema: %s\n", yytext); }
 	YY_BREAK
 case 43:
 YY_RULE_SETUP
-#line 93 ".\\Lexer.l"
+#line 93 ".\\src\\Lexer.l"
 { printf("Token: CERRADURA_ID | Lexema: %s\n", yytext); }
 	YY_BREAK
 case 44:
 YY_RULE_SETUP
-#line 94 ".\\Lexer.l"
+#line 94 ".\\src\\Lexer.l"
 { printf("Token: ALTAVOZ_ID | Lexema: %s\n", yytext); }
 	YY_BREAK
 case 45:
 YY_RULE_SETUP
-#line 95 ".\\Lexer.l"
+#line 95 ".\\src\\Lexer.l"
 { printf("Token: ALARMA_ID | Lexema: %s\n", yytext); }
 	YY_BREAK
 /* Literales y Unidades Compuestas */
 case 46:
 YY_RULE_SETUP
-#line 98 ".\\Lexer.l"
+#line 98 ".\\src\\Lexer.l"
 { printf("Token: TEMPERATURA | Lexema: %s\n", yytext); }
 	YY_BREAK
 case 47:
 YY_RULE_SETUP
-#line 99 ".\\Lexer.l"
+#line 99 ".\\src\\Lexer.l"
 { printf("Token: PORCENTAJE | Lexema: %s\n", yytext); }
 	YY_BREAK
 case 48:
 YY_RULE_SETUP
-#line 100 ".\\Lexer.l"
+#line 100 ".\\src\\Lexer.l"
 { printf("Token: ILUMINANCIA | Lexema: %s\n", yytext); }
 	YY_BREAK
 case 49:
 YY_RULE_SETUP
-#line 101 ".\\Lexer.l"
+#line 101 ".\\src\\Lexer.l"
 { printf("Token: TIEMPO | Lexema: %s\n", yytext); }
 	YY_BREAK
 case 50:
 YY_RULE_SETUP
-#line 102 ".\\Lexer.l"
+#line 102 ".\\src\\Lexer.l"
 { printf("Token: VALOR_HORA | Lexema: %s\n", yytext); }
 	YY_BREAK
 case 51:
 YY_RULE_SETUP
-#line 103 ".\\Lexer.l"
+#line 103 ".\\src\\Lexer.l"
 { printf("Token: VALOR_FECHA | Lexema: %s\n", yytext); }
 	YY_BREAK
 case 52:
 YY_RULE_SETUP
-#line 104 ".\\Lexer.l"
+#line 104 ".\\src\\Lexer.l"
 { printf("Token: EMAIL | Lexema: %s\n", yytext); }
 	YY_BREAK
 case 53:
 YY_RULE_SETUP
-#line 105 ".\\Lexer.l"
+#line 105 ".\\src\\Lexer.l"
 { printf("Token: TEXTO | Lexema: %s\n", yytext); }
 	YY_BREAK
 /* Operadores y Delimitadores */
 case 54:
 YY_RULE_SETUP
-#line 108 ".\\Lexer.l"
+#line 108 ".\\src\\Lexer.l"
 { printf("Token: OP_IGUALDAD | Lexema: %s\n", yytext); }
 	YY_BREAK
 case 55:
 YY_RULE_SETUP
-#line 109 ".\\Lexer.l"
+#line 109 ".\\src\\Lexer.l"
 { printf("Token: OP_RELACIONAL | Lexema: %s\n", yytext); }
 	YY_BREAK
 case 56:
 YY_RULE_SETUP
-#line 110 ".\\Lexer.l"
+#line 110 ".\\src\\Lexer.l"
 { printf("Token: ASIGNACION | Lexema: =\n"); }
 	YY_BREAK
 /* Palabras desconocidas y Control de errores léxicos */
 case 57:
 YY_RULE_SETUP
-#line 113 ".\\Lexer.l"
+#line 113 ".\\src\\Lexer.l"
 { printf("TOKEN_DESCONOCIDO: %s (En linea %d)\n", yytext, linea); }
 	YY_BREAK
 case 58:
 YY_RULE_SETUP
-#line 114 ".\\Lexer.l"
+#line 114 ".\\src\\Lexer.l"
 { printf("TOKEN_ERROR_CARACTER: %s (En linea %d)\n", yytext, linea); }
 	YY_BREAK
 case 59:
 YY_RULE_SETUP
-#line 116 ".\\Lexer.l"
+#line 116 ".\\src\\Lexer.l"
 ECHO;
 	YY_BREAK
 #line 1204 "lex.yy.c"
@@ -2086,7 +2086,7 @@ int main()
 	return 0;
 	}
 #endif
-#line 116 ".\\Lexer.l"
+#line 116 ".\\src\\Lexer.l"
 
 	/*----- Sección de Procedimientos --------*/
 
